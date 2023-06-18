2b808ba9a810163c336f8647a9b4210f4cb33b1c
Iván Sánchez Corral <ivan.sanchezc@opendeusto.es>
2023-06-18 15:40:29 +0200

    Fix code smells on the wrapper library

--- openai/openaiWrapper.js
+++ openai/openaiWrapper.js
@@ -1,8 +1,6 @@
 const { log, debugLevels } = require('../util/logger');
 const { Configuration, OpenAIApi } = require('openai');
 
-const DEBUG_MODE = process.env.DEBUG_MODE;
-
 /**
  * A wrapper class for OpenAI API.
  */
@@ -22,12 +20,17 @@ class OpenAIWrapper {
    * @param {object} chat - The chat object containing the chat history.
    * @returns {object} - The updated chat object.
    */
-  async undoLastCompletion(chat) {   
-    if (chat.messages.length > 2) {
-      chat.messages = chat.messages.slice(0, -2);
-      log(`Done, updated messages: ${JSON.stringify(chat.messages)}`, debugLevels.ALL);
+  async undoLastCompletion(chat) {
+    try {
+      if (chat.messages.length > 2) {
+        chat.messages = chat.messages.slice(0, -2);
+        log(`Done, updated messages: ${JSON.stringify(chat.messages)}`, debugLevels.ALL);
+      }
+      return chat;
+    } catch (error) {
+      log(`Error occurred while undoing completion: ${error}`, debugLevels.BASIC);
+      throw error;
     }
-    return chat;
   }
 
   /**
@@ -39,19 +42,19 @@ class OpenAIWrapper {
   async chatMessageCompletion(chat) {
     if (!chat.messages.length) return chat;
     try {
-      const chat_completion = await this.openai.createChatCompletion({
+      const chatCompletion = await this.openai.createChatCompletion({
         model: chat.model,
         messages: chat.messages,
-        max_tokens: chat.max_tokens,
+        max_tokens: chat.maxTokens,
         temperature: chat.temperature,
         presence_penalty: chat.presence_penalty
       });
-      chat.messages.push(chat_completion.data.choices[0].message);
+      chat.messages.push(chatCompletion.data.choices[0].message);
       return chat;
     } catch (error) {
       log(`OpenAI API error: ${error}`, debugLevels.BASIC);
       throw error;
-    } 
+    }
   }
 
   /**
@@ -60,11 +63,11 @@ class OpenAIWrapper {
    * @returns {object} - The updated chat object.
    * @throws - If there's an error with the OpenAI API.
    */
-  async regenerateLastCompletion(chat){
+  async regenerateLastCompletion(chat) {
     log('Regenerating last completion', debugLevels.BASIC);
     if (chat.messages.length) {
       chat.messages.pop();
-      return await this.chatMessageCompletion(chat);
+      return this.chatMessageCompletion(chat);
     }
     return chat;
   }
@@ -74,21 +77,19 @@ class OpenAIWrapper {
    * @param {Array} messages - The chat history.
    * @param {string} model - The OpenAI model to use for generating the message. Default: 'gpt-3.5-turbo-16k'.
    * @param {number} temperature - Controls the "creativity" of the generated text. Default: 1.0.
-   * @param {number} max_tokens - The maximum number of tokens to generate. Default: 4096.
+   * @param {number} maxTokens - The maximum number of tokens to generate. Default: 4096.
    * @returns {object} - The generated message object.
    * @throws - If there's an error with the OpenAI API.
    */
-
-  async createChatCompletion(messages, model = 'gpt-3.5-turbo-16k', temperature = 1.0, max_tokens = 4096) {
-    if (!messages) return;
-    log(`messages: ${messages}, model: ${model}, temperature: ${temperature}, maxTokens: ${max_tokens}`, debugLevels.NONE);
+  async createChatCompletion(messages, model = 'gpt-3.5-turbo-16k', temperature = 1.0, maxTokens = 4096) {
+    log(`messages: ${messages}, model: ${model}, temperature: ${temperature}, maxTokens: ${maxTokens}`, debugLevels.NONE);
     try {
-      return await this.openai.createChatCompletion({ model, messages, temperature, max_tokens });
+      return await this.openai.createChatCompletion({ model, messages, temperature, max_tokens: maxTokens });
     } catch (error) {
       log(`OpenAI API error: ${error}`, debugLevels.BASIC);
       throw error;
-    } 
-  } 
+    }
+  }
 }
 
 module.exports = OpenAIWrapper;
