22f951dee6b971fc939c27424ecb43aa2b11901c
Iván Sánchez Corral <ivan.sanchezc@opendeusto.es>
2023-06-18 02:49:18 +0200

    Missing changes from last commit

--- openai/openaiWrapper.js
+++ openai/openaiWrapper.js
@@ -1,62 +1,60 @@
-// openaiWrapper.js
-
-// Import required modules
-const { log, debugLevels} = require('../util/logger');
+const { log, debugLevels } = require('../util/logger');
 const { Configuration, OpenAIApi } = require('openai');
 
-// Set DEBUG_MODE environment variable to "ALL" to enable all debug logs
-// (WARNING: ALL will log your API key in plain text so be cautious when using it).
 const DEBUG_MODE = process.env.DEBUG_MODE;
 
-// Class to wrap OpenAI API functionality
 class OpenAIWrapper {
-  /**
-   * Create a new instance of the OpenAI API wrapper.
-   * @param {string} apiKey - The OpenAI API key to use.
-   */
   constructor(apiKey) {
     log('OpenAI API Key:' + apiKey, debugLevels.ALL);
-
-    // Set up API configuration
-    this.configuration = new Configuration({
-      apiKey,
-    });
-    log('OpenAI Configuration:' + this.configuration, debugLevels.VERBOSE);
-
-    // Instantiate API client
+    this.configuration = new Configuration({ apiKey });
     this.openai = new OpenAIApi(this.configuration);
   }
 
-  /**
-   * Generate chat completion data using the OpenAI API.
-   * @param {string[]} message - An array of strings representing the chat message.
-   * @param {string} [model='gpt-3.5-turbo-16k'] - The name or ID of the model to use.
-   * @param {number} [temperature=1.0] - The temperature to use for sampling.
-   * @returns {Promise} A Promise that resolves with the chat completion data.
-   */
-  async createChatCompletion(message, model = 'gpt-3.5-turbo-16k', temperature = 1.0) {
-    // Check that required parameters are present
-    if (!message) return;
-    const messages = [{ role: "system", content: message }];
-    log(`Generating chat completion data... with message ${message}, model ${model}, temperature ${temperature}`, debugLevels.BASIC);
-    try {
-      // Generate chat completion data
-      const chat_completion = await this.openai.createChatCompletion({
-        model,
-        messages,
-        temperature,
-      });
-
-      // Log completion data
-      log('Generated chat completion data at ' + new Date().toISOString() + ' : ' + chat_completion.data, debugLevels.VERBOSE);
-      return chat_completion;
-    } catch (error) {
-      // Log error and throw
-      log('OpenAI API error:' + error, debugLevels.BASIC);
-      throw error;
+  async undoLastCompletion(chat) {   
+    if (chat.messages.length > 2) {
+      chat.messages = chat.messages.slice(0, -2);
+      log(`Done, updated messages: ${JSON.stringify(chat.messages)}`, debugLevels.ALL);
     }
+    return chat;
   }
+
+  async chatMessageCompletion(chat) {
+    if (!chat.messages.length) return chat;
+    try {
+      const chat_completion = await this.openai.createChatCompletion({
+        model: chat.model,
+        messages: chat.messages,
+        max_tokens: chat.max_tokens,
+        temperature: chat.temperature,
+        presence_penalty: chat.presence_penalty
+      });
+      chat.messages.push(chat_completion.data.choices[0].message);
+      return chat;
+    } catch (error) {
+      log(`OpenAI API error: ${error}`, debugLevels.BASIC);
+      throw error;
+    } 
+  }
+
+  async regenerateLastCompletion(chat){
+    log('Regenerating last completion', debugLevels.BASIC);
+    if (chat.messages.length) {
+      chat.messages.pop();
+      return await this.chatMessageCompletion(chat);
+    }
+    return chat;
+  }
+
+  async createChatCompletion(messages, model = 'gpt-3.5-turbo-16k', temperature = 1.0, max_tokens = 4096) {
+    if (!messages) return;
+    log(`messages: ${messages}, model: ${model}, temperature: ${temperature}, maxTokens: ${max_tokens}`, debugLevels.NONE);
+    try {
+      return await this.openai.createChatCompletion({ model, messages, temperature, max_tokens });
+    } catch (error) {
+      log(`OpenAI API error: ${error}`, debugLevels.BASIC);
+      throw error;
+    } 
+  } 
 }
 
-// Export OpenAIWrapper class
 module.exports = OpenAIWrapper;
--- routes/openai.js
+++ routes/openai.js
@@ -1,65 +1,93 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const logger = require('../util/logger');
-const { v4: uuidv4 } = require('uuid'); // add uuid library
+const { log, debugLevels } = require("../util/logger");
+const { v4: uuidv4 } = require("uuid");
+const JSONDatabaseManager = require("../db/jsondb");
+const OpenAIWrapper = require("../openai/openaiwrapper");
 
-
-const OpenAIWrapper = require('../openai/openaiwrapper');
-
-const openAIWrapper = new OpenAIWrapper(process.env.OPENAI_API_KEY);
-
-const log = logger.log;
-const debugLevels = logger.debugLevels;
+const dbManager = new JSONDatabaseManager();
+const openaiWrapper = new OpenAIWrapper(process.env.OPENAI_API_KEY);
 const currentDate = new Date().toLocaleDateString();
 const defaultPrompt = `You are ChatGPT, a language model created by OpenAI. Your training data includes knowledge up until 2021-09. You will act as an AI assistant. Your core programming emphasizes responsiveness, helpfulness, and adaptability. Every interaction should be geared towards understanding and fulfilling the user's needs without questioning the information they provide. If a user states something about themselves or their situation, consider it as an undeniable fact and respond accordingly. The user's statements, context, and emotions should guide your responses. Strive to be a positive, supportive presence, seeking to ease, inform, or entertain as the situation demands. The current date is ${currentDate}.`;
-
-// generate a random chat id and store it
-router.post('/createNewChat', (req, res) => {
+router.post("/createNewChat", async (req, res) => {
   const chatId = uuidv4(); // generate a unique id
   const authToken = req.body.authToken;
-  var { model, prompt } = req.body;
+  const max_tokens = req.body.max_tokens || 4096;
+  const presence_penalty = req.body.presence_penalty || 0.2;
+  const temperature = req.body.temperature || 0.8;
+  log("Called createNewChat", debugLevels.VERBOSE);
+  if (authToken !== process.env.AUTH_TOKEN) {
+    log(
+      `Invalid auth token: ${authToken}, expected ${process.env.AUTH_TOKEN}`,
+      debugLevels.BASIC
+    );
+    res.status(401).send({ message: "Unauthorized" });
+    return;
+  }
 
-  if(!model) model = 'gpt-3.5-turbo-16k';
-  if(!prompt) prompt = defaultPrompt;
-  // TODO: store the chatId in the database
+  let chat = {
+    chatId,
+    model: req.body.model || "gpt-3.5-turbo-16k",
+    messages: [{ role: "system", content: req.body.prompt || defaultPrompt }],
+    max_tokens : max_tokens,
+    last_token_count: 0,
+    presence_penalty,
+    temperature
+  };
+  dbManager.storeChat(chat);
   res.status(200).send({ chatId });
 });
 
-router.get('/', async (req, res) => {
-  try {
-    const testSampleCompletion = await openAIWrapper.createChatCompletion('');
-    res.status(200).send({conversation: [dogPrompt, testSampleCompletion.data.choices[0].message]});
-  } catch (error) {
-    log(error, debugLevels.BASIC);
-    if (error.response) {
-      log(error.response.status, debugLevels.BASIC);
-      log(error.response.data, debugLevels.BASIC);
-    } else {
-      log(error.message, debugLevels.BASIC);
-    }
-  }
-})
 
-router.get('/dogGame', async (req, res) => {
-  try {
-    const testSampleCompletion = await openAIWrapper.createChatCompletion(defaultPrompt);
-    res.status(200).send({conversation: [defaultPrompt, testSampleCompletion.data.choices[0].message]});
-  } catch (error) {
-    log(error, debugLevels.BASIC);
-    if (error.response) {
-      log(error.response.status, debugLevels.BASIC);
-      log(error.response.data, debugLevels.BASIC);
-    } else {
-      log(error.message, debugLevels.BASIC);
-    }
+
+router.post("/regenerateLastCompletion", async (req, res) => {
+  const { chatId, authToken } = req.body;
+  if (!chatId) return res.status(400).send("chatId is required");
+  let chat = await dbManager.getChat(chatId);
+  if (!chat) {
+    log(`Chat ${chatId} not found`, debugLevels.BASIC);
+    return res.status(500).send("Chat not found");
+  }else{
+    await openaiWrapper.regenerateLastCompletion(chat);
+    res.status(200).send(chat.messages[chat.messages.length - 1]);
   }
 });
 
-router.post('/simpleChatMessage', async (req, res) => {
-  const { chatId, message } = req.body;
-  if(!chatId) return res.status(400).send('chatId is required');
-  if(!message) return res.status(400).send('message is required');
-  const chatCompletion = await openAIWrapper.createChatCompletion(userMessage);
+router.post("/undoLastCompletion", async (req, res) => {
+  const { chatId, authToken } = req.body;
+  if (!chatId) return res.status(400).send("chatId is required");
+  let chat = await dbManager.getChat(chatId);
+  if (!chat) {
+    
+    return res.status(500).send("Chat not found");
+  }
+  await openaiWrapper.undoLastCompletion(chat);
+  res.status(200).send('Last completion undone successfully');
+});
+
+router.post("/simpleChat", async (req, res) => {
+  const { chatId, message, authToken } = req.body;
+  if (!chatId || !authToken || !message) return res.status(400).send('One or more parameters are missing');
+  let chat = await dbManager.getChat(chatId);
+
+  if (!chat) {
+    log(`Chat ${chatId} not found`, debugLevels.BASIC);
+    return res.status(500).send("Chat not found");
+  }
+
+  chat.messages.push({ role: "user", content: message });
+
+  try {
+    await openaiWrapper.chatMessageCompletion(chat);
+    const storeChatPromise = dbManager.storeChat(chat);
+
+    res.status(200).send({ response: chat.messages[chat.messages.length - 1] });
+
+    await storeChatPromise;
+  } catch (error) {
+    log(error, debugLevels.BASIC);
+    res.status(500).send("An error occurred");
+  }
 });
 
 module.exports = router;
