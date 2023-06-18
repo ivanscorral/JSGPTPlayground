e1950c7731096bb0221046da9c1f9c59bec80562
Iván Sánchez Corral <ivan.sanchezc@opendeusto.es>
2023-06-18 02:49:04 +0200

    added new endpoints and wrapper functions for chats, implemented chat saving

--- middleware/responseLogger.js
+++ middleware/responseLogger.js
@@ -1,10 +1,8 @@
-// logResponseTime.js
 const { log, debugLevels } = require('../util/logger');
 const responseTime = require("response-time");
 
 const logResponseTime = responseTime((req, res, time) => {
-  console.log(debugLevels.BASIC);
-  log(`${req.method} ${req.originalUrl} ${res.statusCode} ${time}ms`, debugLevels.NONE);
+  log(`${req.method} ${req.originalUrl} ${res.statusCode} ${time}ms`, debugLevels.ALL);
 });
 
 module.exports = { logResponseTime };
